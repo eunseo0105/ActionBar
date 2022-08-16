4d1c9386d2fa808f943543ea2fbea8fcba633900
기은서 <ellen1987@naver.com>
2022-08-16 17:33:22 +0900

    뷰 바인딩 사용하여 코드 수정

--- app/src/main/java/com/es/inflearn2/MainActivity.kt
+++ app/src/main/java/com/es/inflearn2/MainActivity.kt
@@ -1,37 +1,47 @@
 package com.es.inflearn2
 
-import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
-import android.widget.TextView
-import java.util.zip.Inflater
+import com.es.inflearn2.databinding.ActivityMainBinding
+import com.es.inflearn2.databinding.CustomActionbarBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding2:CustomActionbarBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding2= CustomActionbarBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         supportActionBar?.setDisplayShowCustomEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
         supportActionBar?.setDisplayShowHomeEnabled(false)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        //layout을 통해 view를 생성한다.
-        val mainLayout = findViewById<LinearLayout>(R.id.main_layout)
-        val topBar= findViewById<RelativeLayout>(R.id.topBar)
-        val layoutInflater=getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        layoutInflater.inflate(R.layout.custom_actionbar, mainLayout, true) // .inflate(넣어야 할 xml, 넣어야 할 xml을 부풀릴? 공간, true)
 
+        /* 파인드 뷰 방법으로 inflate 구현
+           val mainLayout = findViewById<LinearLayout>(R.id.main_layout)
+           val topBar= findViewById<RelativeLayout>(R.id.topBar)
+           val layoutInflater=getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+           layoutInflater.inflate(R.layout.custom_actionbar, mainLayout, true) // .inflate(넣어야 할 xml, 넣어야 할 xml을 부풀릴? 공간, true)
+           }*/
 
-        val textView2=findViewById<TextView>(R.id.textView2)
-        topBar.run{
-            textView2.text="커스텀 액션바"
-            textView2.setTextColor(Color.BLACK)
+        layoutInflater.inflate(R.layout.custom_actionbar, binding.mainLayout, true)
+
+        /* 파인드 뷰 방법
+         val topBar= findViewById<RelativeLayout>(R.id.topBar)
+         val textView2 = findViewById<TextView>(R.id.textView2)*/
+
+        binding2.topBar.run {
+            binding2.textView2.text = "커스텀 액션바"
+            binding2.textView2.setTextColor(Color.BLACK)
         }
     }
+
 }

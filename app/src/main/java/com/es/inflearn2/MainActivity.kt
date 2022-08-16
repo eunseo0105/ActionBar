4569e2593194d408d6060c37c8f46a59d484f2e3
기은서 <ellen1987@naver.com>
2022-08-16 16:07:11 +0900

    ActionBar inflate 오류 해결

--- app/src/main/java/com/es/inflearn2/MainActivity.kt
+++ app/src/main/java/com/es/inflearn2/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.TextView
 import java.util.zip.Inflater
@@ -15,22 +16,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val textView2=findViewById<TextView>(R.id.textView2)
-
         supportActionBar?.setDisplayShowCustomEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
         supportActionBar?.setDisplayShowHomeEnabled(false)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
         //layout을 통해 view를 생성한다.
+        val mainLayout = findViewById<LinearLayout>(R.id.main_layout)
         val topBar= findViewById<RelativeLayout>(R.id.topBar)
         val layoutInflater=getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        layoutInflater.inflate(R.layout.custom_actionbar,topBar,true)
+        layoutInflater.inflate(R.layout.custom_actionbar, mainLayout, true) // .inflate(넣어야 할 xml, 넣어야 할 xml을 부풀릴? 공간, true)
 
-/*
+
+        val textView2=findViewById<TextView>(R.id.textView2)
         topBar.run{
             textView2.text="커스텀 액션바"
-            textView2.setTextColor(Color.WHITE)
-        }*/
+            textView2.setTextColor(Color.BLACK)
+        }
     }
 }
